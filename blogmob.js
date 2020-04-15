ce26e504d027e33cf8c00b6ce7031626436afb67
ZulFikri <52121057+XuZuLfikri@users.noreply.github.com>
2020-04-15 15:23:39 +0700

    Delete blogmob.js

--- blogmob.js
+++
@@ -1,7 +0,0 @@
-/*!
-  ;;;;;;;;;;;;;;;	 ;;;;;;;	 ;;;;;;;	;;;;;;			©
-  	   ,,;;;;;	 ;;;;;;;	  ;;;;;;	;;;;;;
-  ;;;;;;;""""""	  ;;;;;;	  ;;;;;;	 ;;;;;
-  ;;;;;;;;;;;;;;;	 ;;;;;;;;;;;;;;;;;;;;	;;;;;;;;;;;;;;;;;;
-*/
-document.getElementById("iklan").innerHTML = "<iframe src= https://googleads.g.doubleclick.net/mads/gma?preqs=0&u_sd=1.5&u_w=320&msid="+app+"&cap=a&js=afma-sdk-a-v3.3.0&toar=0&isu=W%27+Math.floor%28Math.random%28%29*9%29+%27EEABB8EE%27+Math.floor%28Math.random%28%29*99%29+%27C2BE770B684D%27+Math.floor%28Math.random%28%29*99999%29+%27ECB&cipa=0&format=320x50_mb&net=wi&app_name=1.android."+app+"&hl=en&u_h=%27+Math.floor%28Math.random%28%29*999%29+%27&carrier=%27+Math.floor%28Math.random%28%29*999999%29+%27&ptime=0&u_audio=4&u_so=p&output=html&region=mobile_app&u_tz=-%27+Math.floor%28Math.random%28%29*999%29+%27&client_sdk=1&ex=1&client="+admob.slice(0, 27)+"&slotname="+admob.slice(28, 38)+"&caps=inlineVideo_interactiveVideo_mraid1_clickTracking_sdkAdmobApiForAds&jsv=18 width= 320px height= 50px>"
